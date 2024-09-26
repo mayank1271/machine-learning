a1e81113a911fcef1a705a52a8f54badb2f09e4e
mayank.manjhi7@gmail.com <mayank.manjhi7@gmail.com>
2024-09-26 12:12:31 +0530

    prediction pipeline

--- src/exception.py
+++ src/exception.py
@@ -1,26 +1,25 @@
 import sys
-import logging
+sys.path.append('E:\machine learning\src')
+from logger import logging
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
 
-def error_message_details(error: Exception) -> str:
-    _, _, exc_tb = sys.exc_info()  # Get exception details
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
-    return f"ERROR occurred in script [{file_name}] line number [{line_number}] error message [{error}]"
+    return error_message
+
+    
 
 class CustomException(Exception):
-    def __init__(self, error: Exception):
-        self.error_message = error_message_details(error)
-        super().__init__(self.error_message)
-
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
     def __str__(self):
         return self.error_message
+    
 
 
-    try:
-        a = 1 / 0
-    except ZeroDivisionError as e:
-        logging.info("Divide by zero error")
-        raise CustomException(e) from e
+        
